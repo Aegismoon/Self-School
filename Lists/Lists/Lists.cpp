bf15f56c8de9b65f03ac93a9a8aa1ccb16ddb607
aegismoon <aegismoon@yandex.ru>
2015-10-18 20:00:38 +0500

    Node and Data structures declared. Compilation is stable: errors - 0, warnings - 0;

---
+++ Lists/Lists/Lists.cpp
@@ -0,0 +1,34 @@
+////////////////////////////////////////////
+//	Concole application C++		          //
+//					                      //
+//	       <LISTS>                        //
+//					                      //
+//	 Author Dmitry Murat		          //
+///////////////////////////////////////////
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+// data structure
+struct Data
+{
+    int key;
+};
+
+// list element - node
+struct Node
+{
+    Data datum;
+    Node* next;
+};
+
+int main( int argc, char* argv[])
+{
+
+    system("pause");
+    return 0;
+}
