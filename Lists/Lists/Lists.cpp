f81ba8f4c6e848966b026af84465162fb2fa1cb8
aegismoon <aegismoon@yandex.ru>
2015-10-18 20:15:40 +0500

    List of elemetns with container mass[5] established

--- Lists/Lists/Lists.cpp
+++ Lists/Lists/Lists.cpp
@@ -28,6 +28,26 @@ struct Node
 
 int main( int argc, char* argv[])
 {
+    srand(time(NULL));
+    Node* begin = NULL;
+    begin = new Node;
+
+    begin->datum.key = 35;
+    begin->next = NULL;
+
+    Data massdata[5] = {1,2,3,4,5};
+
+    Node *end = begin;
+    for (int i(0); i < 5 ; i++ )
+    {
+        end->next = new Node;
+        end = end->next;
+        end->datum = massdata[i];
+        end->next = NULL;
+    }
+
+
+
 
     system("pause");
     return 0;
